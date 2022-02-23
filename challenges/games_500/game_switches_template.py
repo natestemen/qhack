22cd05084c6b72fbd00d8278b08689e46154acc9
YuNariai <98194200+YuNariai@users.noreply.github.com>
2022-02-23 01:02:48 -0500

    Add files via upload

---
+++ challenges/games_500/game_switches_template.py
@@ -0,0 +1,66 @@
+#! /usr/bin/python3
+
+import sys
+from pennylane import numpy as np
+import pennylane as qml
+
+
+def switch(oracle):
+    """Function that, given an oracle, returns a list of switches that work by executing a
+    single circuit with a single shot. The code you write for this challenge should be completely
+    contained within this function between the # QHACK # comment markers.
+
+    Args:
+        - oracle (function): oracle that simulates the behavior of the lights.
+
+    Returns:
+        - (list(int)): List with the switches that work. Example: [0,2].
+    """
+
+    dev = qml.device("default.qubit", wires=[0, 1, 2, "light"], shots=1)
+
+    @qml.qnode(dev)
+    def circuit():
+        
+        # QHACK #
+
+        qml.Hadamard(wires=0)
+        qml.Hadamard(wires=1)
+        qml.Hadamard(wires=2)
+        
+        qml.PauliX(wires="light")
+        qml.Hadamard(wires="light")
+
+        
+        oracle()
+
+        qml.Hadamard(wires=0)
+        qml.Hadamard(wires=1)
+        qml.Hadamard(wires=2)
+        
+        # QHACK #
+
+        return qml.sample(wires=range(3))
+
+    sample = np.array(circuit())
+    indexs = np.where(sample == 1)
+
+    # QHACK #
+
+    return list(indexs[0])
+
+    # QHACK #
+
+
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
+    inputs = sys.stdin.read().split(",")
+    numbers = [int(i) for i in inputs]
+
+    def oracle():
+        for i in numbers:
+            qml.CNOT(wires=[i, "light"])
+
+    output = switch(oracle)
+    print(*output, sep=",")
+
