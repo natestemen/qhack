a73af0c1ac025b15e77952069e2de6c642cee7b0
YuNariai <98194200+YuNariai@users.noreply.github.com>
2022-02-25 05:51:11 -0500

    Update ising_classifier.py
    output is not int

--- challenges/qml_300/ising_classifier.py
+++ challenges/qml_300/ising_classifier.py
@@ -74,8 +74,22 @@ def classify_ising_data(ising_configs, labels):
             qml.RY(configs[i]*params[i] + params[i+num_wires],wires=[i])
         for j in range(1,num_wires):
             qml.CNOT(wires=[j-1,j])
+        qml.CNOT(wires=[j,0])
+        
+        for i1 in range(num_wires):
+            qml.RY(configs[i1]*params[i1] + params[i1+num_wires],wires=[i1])
+        for j1 in range(1,num_wires):
+            qml.CNOT(wires=[j1-1,j1])
+        qml.CNOT(wires=[j1,0])
+        
+        for i2 in range(num_wires):
+            qml.RY(configs[i2]*params[i2] + params[i2+num_wires],wires=[i2])
+        for j2 in range(1,num_wires):
+            qml.CNOT(wires=[j2-1,j2])
+        qml.CNOT(wires=[j2,0])
+        
             
-        return qml.probs(wires=num_wires-1)
+        return qml.probs(wires=3)
     
     # Define a cost function below with your needed arguments
     def cost(params):
@@ -83,14 +97,14 @@ def classify_ising_data(ising_configs, labels):
         # QHACK #
         
         Y = labels
-        
-        predictions = np.zeros(DATA_SIZE)
-        
-        for k in range(DATA_SIZE):
-             
+        #predictions = np.zeros(5) 
+        predictions=np.array([])
+        for k in range(DATA_SIZE):             
             prob = circuit(ising_configs[k],params)[0]
-            predictions[k] = np.around(prob*2-1)
-
+            
+            #predictions[k] = np.around(prob*2-1)
+            predictions=np.append(predictions,prob*2-1)
+        # QHACK #
              
 
         # QHACK #
@@ -100,37 +114,36 @@ def classify_ising_data(ising_configs, labels):
 
 
     init_params = np.array([1.,1.,1.,1.,0.1,0.1,0.1,0.1])
-    opt = qml.AdamOptimizer(stepsize=0.1)
-    steps = 3
+    opt = qml.AdagradOptimizer(stepsize=0.8)
+    steps = 10
     params = init_params
     
 
     for m in range(steps):
-        #print(cost(params))
         params = opt.step(cost, params)
         
 
     # QHACK #
- 
+
     predictions = np.zeros(DATA_SIZE)
     for kk in range(DATA_SIZE):
-         predictions[kk] = np.around(circuit(ising_configs[kk],params)[0])
+         prediction = np.around(circuit(ising_configs[kk],params)[0])*2-1
+         predictions[kk] = prediction.astype(int)
     return predictions
-    #return params
+    #return accuracy(labels, predictions)
 
 
 if __name__ == "__main__":
     '''inputs = np.array(
-        sys.stdin.read().split(","), dtype=int, requires_grad=False
+        sys.stdin.read().split(","), dtype=int, requires_grad=True
     ).reshape(DATA_SIZE, -1)'''
     inputs = np.array(
         [0,0,1,0,-1,0,0,0,0,1,0,0,1,0,-1,0,0,0,0,1,0,0,0,0,-1,1,1,0,1,-1,0,0,1,0,-1,1,0,0,1,-1,0,0,0,0,1,1,1,1,1,1,1,0,1,0,-1,0,1,0,1,-1,1,0,0,1,-1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,-1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,-1,0,1,0,1,-1,1,1,1,1,1,0,1,1,0,-1,1,0,1,1,-1,0,0,0,1,-1,0,0,1,0,-1,1,0,0,0,-1,0,0,0,0,1,0,0,0,1,-1,0,0,1,1,-1,0,0,0,0,1,1,1,1,0,-1,1,1,1,1,1,1,0,1,0,-1,1,0,0,0,-1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,0,0,-1,1,1,1,1,1,0,0,0,1,-1,1,1,1,1,1,1,1,0,1,-1,0,1,0,0,-1,1,1,1,1,1,0,0,1,1,-1,0,0,0,0,1,1,0,0,0,-1,0,0,0,0,1,1,1,1,1,1,1,1,1,0,-1,0,0,0,0,1,1,1,0,1,-1,0,0,0,0,1,1,0,1,0,-1,1,1,0,1,-1,0,0,0,0,1,1,1,1,1,-1,1,1,1,1,1,1,0,0,1,-1,0,0,0,0,1,0,0,0,1,-1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,-1,1,0,1,1,-1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,-1,0,0,0,0,1,0,1,0,0,-1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,0,0,-1,1,1,1,1,1,0,0,0,0,1,0,0,1,1,-1,0,1,1,1,-1,0,1,1,0,-1,1,0,0,0,-1,1,1,1,1,1,0,1,1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,-1,0,0,0,0,1,1,0,1,0,-1,0,0,0,0,1,1,0,0,1,-1,0,1,1,1,-1,1,0,0,0,-1,0,0,0,0,1,1,1,1,1,-1,1,1,1,0,-1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1,1,1,0,-1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,-1,1,0,0,1,-1,0,0,1,1,-1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,0,0,1,-1,1,1,1,1,1,0,1,0,1,-1,0,1,0,0,-1,0,0,0,0,1,0,0,0,0,1,0,0,1,1,-1,0,0,1,0,-1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,-1,1,1,1,1,1,0,0,0,0,1,1,0,0,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,-1,0,1,0,0,-1,1,1,1,1,1,0,0,0,0,-1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,-1,1,1,1,1,1,0,1,0,0,-1,1,1,1,1,1,0,0,1,0,-1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0,1,1,0,-1,0,0,0,0,1,0,1,0,0,-1,1,0,1,0,-1,0,1,1,0,-1,1,1,0,0,-1,0,1,1,0,-1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,-1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,-1,0,0,0,0,1,1,0,0,1,-1,1,1,0,0,-1,0,0,0,0,-1,0,0,0,0,1,0,0,0,1,-1,1,1,1,1,1,1,0,0,1,-1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1,0,0,-1,0,0,0,0,1,1,0,0,1,-1,1,0,0,0,-1,0,0,0,0,1,1,0,0,0,-1,1,0,0,1,-1,1,0,0,0,-1,1,0,0,1,-1,1,1,0,1,-1,0,0,0,0,1,0,1,0,1,-1,0,0,1,1,-1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,0,0,0,-1,0,1,1,1,-1,0,0,0,0,1,1,0,0,0,-1,0,1,1,0,-1,0,0,0,0,-1,0,0,0,0,1,1,1,1,1,1,1,1,0,1,-1,0,0,0,0,1,0,0,1,1,-1,1,1,1,1,-1,0,0,0,0,-1,0,0,0,0,1,1,0,0,0,-1,0,0,0,0,1,1,1,1,1,1,0,0,1,0,-1,1,0,0,0,-1,0,0,0,0,1,1,0,1,0,-1,1,0,0,1,-1,0,0,1,1,-1,1,1,0,0,-1,1,0,1,1,-1,0,0,0,0,1,1,1,1,1,1,1,1,0,1,-1,1,1,1,1,1,1,0,0,1,-1,1,0,0,0,-1,0,0,0,0,1,0,0,0,0,-1,1,0,0,1,-1,1,1,1,1,-1,0,0,0,0,-1,1,1,0,0,-1,1,1,1,1,1,1,1,1,1,-1,0,0,0,0,1,0,0,0,1,-1,0,1,1,1,-1,1,1,1,1,1,0,0,0,0,-1,1,0,1,1,-1,1,1,1,0,-1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,0,0,-1
-    ], dtype=int, requires_grad=False
+    ], dtype=int, requires_grad=True
     ).reshape(DATA_SIZE, -1)
     
     ising_configs = inputs[:, :-1]
     labels = inputs[:, -1]
     predictions = classify_ising_data(ising_configs, labels)
-    #print(*predictions, sep=",")
-    print(predictions)
+    print(*predictions, sep=",")
     
